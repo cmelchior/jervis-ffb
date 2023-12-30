37affa0f08b5d473e0b2ce064af3175321a95c1c
Christian Melchior <christian.melchior@mongodb.com>
2023-12-30 13:01:37 +0100

    Add dummy procedures for all Prayers of Nuffle results

---
+++ modules/game-model/src/commonMain/kotlin/dk/ilios/jervis/procedures/bb2020.prayersofnuffle/BlessedStatueOfNuffle.kt
@@ -0,0 +1,32 @@
+package dk.ilios.jervis.procedures.bb2020.prayersofnuffle
+
+import compositeCommandOf
+import dk.ilios.jervis.commands.Command
+import dk.ilios.jervis.commands.ExitProcedure
+import dk.ilios.jervis.fsm.ComputationNode
+import dk.ilios.jervis.fsm.Node
+import dk.ilios.jervis.fsm.Procedure
+import dk.ilios.jervis.reports.LogCategory
+import dk.ilios.jervis.reports.SimpleLogEntry
+import dk.ilios.jervis.model.Game
+import dk.ilios.jervis.rules.Rules
+
+/**
+ * Procedure for handling the Prayer of Nuffle "Blessed Statue of Nuffle" as described on page 39
+ * of the rulebook.
+ */
+object BlessedStatueOfNuffle: Procedure() {
+    override val initialNode: Node = ApplyEvent
+    override fun onEnterProcedure(state: Game, rules: Rules): Command? = null
+    override fun onExitProcedure(state: Game, rules: Rules): Command? = null
+
+    object ApplyEvent: ComputationNode() {
+        // TODO Figure out how to do this
+        override fun apply(state: Game, rules: Rules): Command {
+            return compositeCommandOf(
+                SimpleLogEntry("Do Blessed Statue of Nuffle!", category = LogCategory.GAME_PROGRESS),
+                ExitProcedure()
+            )
+        }
+    }
+}
--- modules/game-model/src/commonMain/kotlin/dk/ilios/jervis/rules/Rules.kt
+++ modules/game-model/src/commonMain/kotlin/dk/ilios/jervis/rules/Rules.kt
@@ -21,6 +21,22 @@ import dk.ilios.jervis.procedures.bb2020.kickoff.PitchInvasion
 import dk.ilios.jervis.procedures.bb2020.kickoff.QuickSnap
 import dk.ilios.jervis.procedures.bb2020.kickoff.SolidDefense
 import dk.ilios.jervis.procedures.bb2020.kickoff.TimeOut
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.BadHabits
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.BlessedStatueOfNuffle
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.FanInteraction
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.FoulingFrenzy
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.FriendsWithTheRef
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.GreasyCleats
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.IntensiveTraining
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.IronMan
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.KnuckleDusters
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.MolesUnderThePitch
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.NecessaryViolence
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.PerfectPassing
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.Stiletto
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.ThrowARock
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.TreacherousTrapdoor
+import dk.ilios.jervis.procedures.bb2020.prayersofnuffle.UnderScrutiny
 import dk.ilios.jervis.utils.INVALID_GAME_STATE
 import kotlin.math.PI
 import kotlin.math.cos
@@ -149,26 +165,25 @@ data class TableResult(val name: String, val procedure: Procedure)
  */
 object PrayersToNuffleTable {
     private val table = mapOf(
-        1 to TableResult("Treacherous Trapdor", DummyProcedure),
-        2 to TableResult("Friends with the Ref", DummyProcedure),
-        3 to TableResult("Stilleto", DummyProcedure),
-        4 to TableResult("Iron Man", DummyProcedure),
-        5 to TableResult("Knuckle Dusters", DummyProcedure),
-        6 to TableResult("Bad Habits", DummyProcedure),
-        7 to TableResult("Greasy Cleats", DummyProcedure),
-        8 to TableResult("Blessed Statue of Nuffle", DummyProcedure),
-        9 to TableResult("Moles unde the Pitch", DummyProcedure),
-        10 to TableResult("Perfect Passing", DummyProcedure),
-        11 to TableResult("Fan Interaction", DummyProcedure),
-        12 to TableResult("Necessary Violence", DummyProcedure),
-        13 to TableResult("Fouling Frenzy", DummyProcedure),
-        14 to TableResult("Throw a Rock", DummyProcedure),
-        15 to TableResult("Under Scrutiny", DummyProcedure),
-        16 to TableResult("Intensive Training", DummyProcedure),
+        1 to TableResult("Treacherous Trapdor", TreacherousTrapdoor),
+        2 to TableResult("Friends with the Ref", FriendsWithTheRef),
+        3 to TableResult("Stiletto", Stiletto),
+        4 to TableResult("Iron Man", IronMan),
+        5 to TableResult("Knuckle Dusters", KnuckleDusters),
+        6 to TableResult("Bad Habits", BadHabits),
+        7 to TableResult("Greasy Cleats", GreasyCleats),
+        8 to TableResult("Blessed Statue of Nuffle", BlessedStatueOfNuffle),
+        9 to TableResult("Moles under the Pitch", MolesUnderThePitch),
+        10 to TableResult("Perfect Passing", PerfectPassing),
+        11 to TableResult("Fan Interaction", FanInteraction),
+        12 to TableResult("Necessary Violence", NecessaryViolence),
+        13 to TableResult("Fouling Frenzy", FoulingFrenzy),
+        14 to TableResult("Throw a Rock", ThrowARock),
+        15 to TableResult("Under Scrutiny", UnderScrutiny),
+        16 to TableResult("Intensive Training", IntensiveTraining),
     )
     /**
-     *
-     * Roll on the table and return the result.
+     * Roll on the Prayers of Nuffle table and return the result.
      */
     fun roll(d16: D16Result): TableResult {
         return table[d16.result] ?: INVALID_GAME_STATE("${d16.result} was not found in the Kick-Off Event Table.")
