d432e86f926c590e0dbceb2da2b4e3c2d48720c2
Christian Melchior <christian.melchior@jetbrains.com>
2025-03-16 10:13:15 +0100

    Fix log output when rolling on the Weather Table

--- modules/jervis-engine/src/commonMain/kotlin/com/jervisffb/engine/reports/ReportWeatherResult.kt
+++ modules/jervis-engine/src/commonMain/kotlin/com/jervisffb/engine/reports/ReportWeatherResult.kt
@@ -6,7 +6,7 @@ class ReportWeatherResult(weather: Weather) : LogEntry() {
     override val category: LogCategory = LogCategory.GAME_PROGRESS
     override val message: String = buildString {
         if (weather != Weather.PERFECT_CONDITIONS) {
-            appendLine("Weather is ${weather.description}")
+            appendLine("Weather is ${weather.title}")
             append(weather.description)
         } else {
             append("Weather is ${weather.description}")
